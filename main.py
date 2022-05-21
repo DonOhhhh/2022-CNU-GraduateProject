61c050e04bfa51f5bda691c621500f7b4fa65f4b
DonOh <201904014@o.cnu.ac.kr>
2022-05-22 08:45:06 +0900

    skeleton code

---
+++ main.py
@@ -0,0 +1,18 @@
+def modifyManager():
+    return None
+
+def buildManager():
+    return None
+
+def performanceManager():
+    return None
+
+def main():
+
+    modifyManager()
+    buildManager()
+    performanceManager()
+
+
+if __name__=='__main__':
+    main()

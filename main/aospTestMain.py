188cd88116828a1db148f94f807fbd78509e29e0
DonOh <201904014@o.cnu.ac.kr>
2022-05-22 15:38:11 +0900

    venv added insert code added testCase added requirement added

--- main/aospTestMain.py
+++ main/aospTestMain.py
@@ -1,10 +1,15 @@
 class modifyManager():
     def __init__(self):
+        self.code = 'testtesttest\n'
         pass
 
+    # os.walk() 함수를 사용하면 편합니다.
     def getFilePath(self,fname):
         return None
 
+    # abcAfter.txt의 2번째 줄에 code를 삽입해주시면 됩니다.
+    # 만약 수정이 잘못되었다면 abcBefore에서 복사해서 다시 수정하시면 됩니다.
+    # ubuntu에서도 진행해보시고 실제 aosp 코드에 주석으로 코드를 삽입했을 때 권한문제가 없는지도 확인해주세요
     def modify(self,fpath):
         return None
 
@@ -13,6 +18,8 @@ class buildManager():
     def __init__(self):
         pass
 
+    # 쉘 스크립트로 빌드하고 emulator 올리는 것까지 작성해주세요
+    # 권한 문제가 없는지 확인해주세요
     def buildAOSP(self):
         return None
 
--- main/testMain.py
+++ main/testMain.py
@@ -8,15 +8,20 @@ class MyTest(unittest.TestCase):
         self.performance = atm.performanceManager()
 
     def test_modify(self):
-        filePath = self.modify.getFilePath(fname="")
+        filePath = self.modify.getFilePath(fname="abcBefore.txt")
         self.assertEqual(filePath, "")
-        with open(filePath, 'r') as f:
-            data1 = f.read()
-        # data1에 코드 삽입 추가
-        #
+        # abcBefore의 데이터의 2번째 줄에 code 삽입
+        data1 = b''
+        with open('abcBefore.txt', 'r') as f:
+            for i,line in enumerate(f.readlines()):
+                if i==2:
+                    data1 += self.modify.code
+                data1 += line
+        # abcAfter 파일의 2번째 줄에 code 삽입
         self.modify.modify(fpath=filePath)
         with open(filePath, 'r') as f:
             data2 = f.read()
+        # 미리 수정된 abcBefore와 modifyManager()가 수정한 abcAfter의 데이터를 비교
         self.assertEqual(data1, data2)
 
     def test_performance(self):

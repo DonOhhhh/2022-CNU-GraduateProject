a8443cd78b9015c39a737235ae106796dd6f20cd
DonOh <201904014@o.cnu.ac.kr>
2022-05-26 06:46:07 +0900

    performance manager implemented by 201904014

--- main/aospTestMain.py
+++ main/aospTestMain.py
@@ -1,3 +1,5 @@
+import frida, sys
+
 class modifyManager():
     def __init__(self):
         self.code = 'testtesttest\n'
@@ -26,13 +28,66 @@ class buildManager():
 
 class performanceManager():
     def __init__(self):
-        pass
+        self.message = ''
+        self.className = ''
+        self.methodName = ''
+        self.Hook_package = ''
+        self.device = frida.get_usb_device(timeout=10)
+        self.measureExecTime()
+        print(self.message)
 
-    def getPID(self,packageName):
-        return None
+    def on_message(self, message, data):
+        if message['type'] == 'send':
+            self.message = message['payload']
+            print(f'package : {self.Hook_package}, class : {self.className}, method : {self.methodName}, exec_time: {self.message}s')
 
-    def measureExecTime(self,pid,func):
-        return None
+    def get_pid(self,Hook_package):
+        pid = self.device.spawn([Hook_package])
+        return pid
+
+    def measureExecTime(self):
+        try:
+            # 작성된 후킹 스크립트 코드 로드 후 프로세스에 연결
+            # Hooking 하는 대상 패키지 이름(앱의 본명)
+            # "kr.ac.cnu.computer.homework10"
+            # self.Hook_package = input('Hooking 하는 대상 패키지 이름(앱의 본명): ')
+            self.Hook_package = 'kr.ac.cnu.computer.homework10'
+            pid = self.get_pid(self.Hook_package)
+            print("App is starting.. pid:{}".format(pid))
+            process = self.device.attach(pid)
+            self.device.resume(pid)
+
+            # android.media.MediaPlayer
+            # self.className = input('Hooking 하고자 하는 Class Name: ')
+            self.className = 'android.media.MediaPlayer'
+            # start
+            # self.methodName = input('Hooking 하고자 하는 Method Name: ')
+            self.methodName = 'start'
+
+            # 후킹 JS 코드
+            jscode = """
+            Java.perform(function() {
+                var classObj = Java.use("%s");
+                classObj.%s.implementation = function() {
+                    var nStart = new Date().getTime();
+                    var retval = this.%s();
+                    var nEnd =  new Date().getTime();
+                    var nDiff = nEnd - nStart;
+                    send(nDiff);
+                    // console.log("Execute time: " + nDiff / 1000.0 + "(s)");
+                    return retval;
+                }
+            });
+            """ % (self.className, self.methodName, self.methodName)
+            # print(jscode)
+            script = process.create_script(jscode)
+            script.on('message', self.on_message)
+            print("[-] Running FR1DA!")
+            script.load()
+            sys.stdin.read()
+
+        except Exception as e:
+            print(e)
 
 
 def main():
@@ -40,7 +95,7 @@ def main():
     print('2. buildManager')
     print('3. performanceManager')
     n = input('input number to test:')
-
+    
     if n == 1:
         obj = modifyManager()
     elif n == 2:

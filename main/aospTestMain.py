4fd03259c78a9ea64d1e4a43886b9af52ac04528
jhc365 <jk365a@naver.com>
2022-05-28 22:08:34 +0900

    modifymanager 개선

--- main/aospTestMain.py
+++ main/aospTestMain.py
@@ -3,6 +3,12 @@ import os
 class modifyManager():#modify
     def __init__(self):
         self.code = 'testtesttest\n'
+
+        self.imp = """import java.io.OutputStream;\nimport java.net.Socket;\nimport java.net.UnknownHostException;\n"""
+
+        self.Zstart = """OutputStream outStream = null;\ntry{\nSocket sk = new Socket("10.0.2.2" , 8000) ;\noutStream = sk.getOutputStream();\nString startmessage = "Zend";\noutStream.write(startmessage.getBytes());\noutStream.flush();\n}catch(UnknownHostException e){\ne.printStackTrace();\n}catch (IOException e) {\ne.printStackTrace();\n}\n"""
+
+        self.Zend = """try{\nString endmessage = "Zend";\noutStream.write(endmessage.getBytes());\noutStream.flush();\n}catch(UnknownHostException e){\ne.printStackTrace();\n}catch (IOException e) {\ne.printStackTrace();\n}\n"""
         pass
 
     # os.walk() 함수를 사용하면 편합니다.
@@ -22,16 +28,25 @@ class modifyManager():#modify
     # 만약 수정이 잘못되었다면 abcBefore에서 복사해서 다시 수정하시면 됩니다.
     # ubuntu에서도 진행해보시고 실제 aosp 코드에 주석으로 코드를 삽입했을 때 권한문제가 없는지도 확인해주세요
     def modify(self,fpath): #ZygoteInit.java 파일에 소켓 통신을 통한 성능측정 코드 추가
-        #소켓 통신 서버 ip,포트 지정
-        serverIp = "0.0.0.0"
-        portNum = "8888"
-
         fpath = self.getFilePath("ZygoteInit.java")
         if fpath == None:
             print("ZygoteINit.java 파일이 존재하지 않습니다.")
             return None
         else:
             print(f'ZygoteINit.java 경로: {fpath}')
+
+        print("ZygoteInit 코드 내 Zygote start 소켓 통신 위치 설정")
+        ZstartMethod = input("코드 내 함수(문자열)을 입력하십시오 :")
+        print("코드 위치를 입력하십시오")
+        ZSMlocation = input("1.위 2.아래 :")
+
+        print("ZygoteInit 코드 내 Zygote end 소켓 통신 위치 설정")
+        ZendMethod = input("코드 내 함수(문자열)을 입력하십시오 :")
+        print("코드 위치를 입력하십시오")
+        ZEMlocation = input("1.위 2.아래 :")
+        if not (((ZSMlocation == "1") | (ZSMlocation == "2"))&((ZEMlocation == "1") | (ZEMlocation == "2"))):
+            print("코드 위치 선택 값이 1 또는 2가 아닙니다.")
+            return
 
         print("zygote 코드 수정을 시작합니다")
 
@@ -47,36 +62,49 @@ class modifyManager():#modify
         print("ZygoteInit.java 파일이 현재 폴더에 백업되었습니다.")
         fb.close()
 
+        import_inserted = False #import문 삽입 여부
+        ZS_inserted = False #Zygote 시작 소켓 삽입 여부
+        ZE_inserted = True#Zygote 종료 소켓 삽입 여부
+
         with open(fpath, 'w') as f:
-            ###소켓 통신 위한 파일 import 추가
-            print("import문 삽입")
-            f.write("import java.io.OutputStream;\n")#서버로 전송만을 위한 ouput 스트림
-            f.write("import java.net.Socket;\n")
-            f.write("import java.net.UnknownHostException;\n")
-
             for codeline in codelines:#읽은 파일 한줄씩 비교/ 쓰기
+                if not import_inserted:
+                    if "import" in codeline:
+                        ###소켓 통신 위한 파일 import 추가
+                        print("import문 삽입")
+                        f.write(self.imp)  # 서버로 전송만을 위한 ouput 스트림
+                        f.write(codeline)  # 기존 파일 코드 작성
+                        import_inserted = True
+                        continue
 
-                if "runSelectLoop" in codeline:#runselectloop 메소드 앞에서 zygote 종료 전송
-                    print ("zygote end 통신코드 삽입")
-                    f.write(f'String endmessage = "Zend";\n')#프로세스 생성 종료 메시지
-                    f.write(f'outStream.write(endmessage.getBytes());\n')
-                    f.write(f'outStream.flush();\n')
-
-
-                f.write(codeline)#기존 파일 코드 작성
-
-                if "main(String argv[])" in codeline: #메인문 탐색, 메인문에에 코드 삽입
+                if ZstartMethod in codeline: #메인문 탐색, 메인문에에 코드 삽입
                     ### 소켓통신시작 및 Zygote 시작 알리는 통신 실시
                     print("소켓통신 시작 코드 삽입")
-                    f.write(f'Socket sk = new Socket("{serverIp}" , {portNum}) ;\n')#소켓 연결
-                    f.write(f'OutputStream outStream = sk.getOutputStream();\n')#전송용 outputstream
-                    f.write(f'String startmessage = "Zend";\n')#생성 종료 메시지
-                    f.write(f'outStream.write(startmessage.getBytes());\n')
-                    f.write(f'outStream.flush();\n')
+                    if ZSMlocation == "1":
+                        f.write(self.Zstart)#소켓 연결 및 Zygote 시작 알림
+                        f.write(codeline)  # 기존 파일 코드 작성
+                    else:
+                        f.write(codeline)  # 기존 파일 코드 작성
+                        f.write(self.Zstart)  # 소켓 연결 및 Zygote 시작 알림
+                        ZS_inserted = True
+                        continue
 
+                if ZendMethod in codeline:#runselectloop 메소드 앞에서 zygote 종료 전송
+                    print ("zygote end 통신코드 삽입")
+                    if ZEMlocation == "1":
+                        f.write(self.Zend)#프로세스 생성 종료 메시지
+                        f.write(codeline)  # 기존 파일 코드 작성
+                    else:
+                        f.write(codeline)  # 기존 파일 코드 작성
+                        f.write(self.Zend)  # 프로세스 생성 종료 메시지
+                        ZE_inserted = True
+                        continue
 
+                f.write(codeline) #기존 파일 코드 작성
 
         f.close()
+        if not (import_inserted & ZS_inserted & ZE_inserted):
+            print("실패: 해당 함수 문자열 혹은 import문 위치를 찾을 수 없습니다.")
 
 
         return None

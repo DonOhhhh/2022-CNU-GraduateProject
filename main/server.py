96938e2786517f3270259c9d028903024b631139
DonOh <201904014@o.cnu.ac.kr>
2022-06-05 17:09:52 +0900

    added log

--- main/server.py
+++ main/server.py
@@ -8,15 +8,23 @@ end = 0
 while True:
     try:
         if start != 0 and end != 0:
+            print('Recorded Zygote init time!')
             with open('result.txt','a') as f:
                 f.write(f'ZygoteInit 실행시간 : {end - start}ms')
             start = 0
             end = 0
+            
         data, client = sock.recvfrom(1500)
+
         if start == 0:
             start = data.decode('utf-8')
-        elif end == 0:
+            print(f'Started time : {start}ms')
+            continue
+
+        if end == 0:
             end == data.decode('utf-8')
+            print(f'Ended time : {end}ms')
+            continue
     except KeyboardInterrupt:
         print(f'Shutting down... {sock}')
         break

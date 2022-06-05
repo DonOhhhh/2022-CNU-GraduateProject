b003cf7155ecb2a0f4226ba1f834e980e5d7a661
DonOh <201904014@o.cnu.ac.kr>
2022-06-05 20:24:28 +0900

    modified shell script

--- main/buildScript.sh
+++ main/buildScript.sh
@@ -4,10 +4,10 @@ path=$1
 option=$2
 
 cd ${path};
+echo ${path};
 
 if [ ${option} = 1 ]; then
   sudo chmod -R 777 .;
-  make clobber;
   . build/envsetup.sh;
   lunch sdk_phone_x86_64-userdebug;
   m;

6254c56de3d8a84efbf44b0f27ef231f6a8d2ba4
DonOh <201904014@o.cnu.ac.kr>
2022-06-05 17:04:51 +0900

    change a mistake

--- main/server.py
+++ main/server.py
@@ -7,16 +7,16 @@ start = 0
 end = 0
 while True:
     try:
-        data, client = sock.recvfrom(1500)
-        if start == 0:
-            start = data.decode('utf-8')
-        elif end == 0:
-            end == data.decode('utf-8')
         if start != 0 and end != 0:
             with open('result.txt','a') as f:
                 f.write(f'ZygoteInit 실행시간 : {end - start}ms')
             start = 0
             end = 0
+        data, client = sock.recvfrom(1500)
+        if start == 0:
+            start = data.decode('utf-8')
+        elif end == 0:
+            end == data.decode('utf-8')
     except KeyboardInterrupt:
         print(f'Shutting down... {sock}')
         break

a81135fd9d6b1277b4f30fd268fbfb44694424f3
DonOh <201904014@o.cnu.ac.kr>
2022-06-05 20:05:58 +0900

    backup

--- main/aospTestMain.py
+++ main/aospTestMain.py
@@ -5,7 +5,8 @@ class modifyManager():
     def __init__(self):
         self.imp = "import java.net.*;\n"
         #UDP 통신 코드
-        self.UDPSock = 'try {\nbyte[] buffer = String.valueOf(System.currentTimeMillis()).getBytes();\nnew DatagramSocket(5000).send(new DatagramPacket(buffer, buffer.length, InetAddress.getByName("10.0.2.2"), 5001));\n} catch (IOException e) { }\n'
+        self.UDPSock = "try {\nbyte[] buffer = (\"end \" + String.valueOf(System.currentTimeMillis())).getBytes();\nnew DatagramSocket(5000).send(new DatagramPacket(buffer, buffer.length, InetAddress.getByName(\"10.0.2.2\"), 5001));\n} catch (IOException e) {}\n"
+        
         pass
 
     def getJsonObjects(self, jfname):#{fname} json 파일을 읽어 객체를 반환
--- main/server.py
+++ main/server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, json
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('localhost', 5001))
@@ -7,24 +7,21 @@ start = 0
 end = 0
 while True:
     try:
-        if start != 0 and end != 0:
+        if start and end:
             print('Recorded Zygote init time!')
             with open('result.txt','a') as f:
-                f.write(f'ZygoteInit 실행시간 : {end - start}ms')
+                f.write(f'ZygoteInit exec time : {end - start}ms')
             start = 0
             end = 0
-            
+
         data, client = sock.recvfrom(1500)
-
-        if start == 0:
-            start = data.decode('utf-8')
-            print(f'Started time : {start}ms')
-            continue
-
-        if end == 0:
-            end == data.decode('utf-8')
-            print(f'Ended time : {end}ms')
-            continue
+        flag, time = data.decode('utf-8').split(' ')
+        if flag == 'start':
+            start = int(time)
+            print(f'Zygote started time : {start}')
+        elif flag == 'end':
+            end = int(time)
+            print(f'Zygote ended time : {end}')
     except KeyboardInterrupt:
         print(f'Shutting down... {sock}')
         break
